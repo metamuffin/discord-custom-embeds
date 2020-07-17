4b2e835e3774c9cfb7d933c5dcf2b30212ec15bb
MetaMuffin <metamuffin@yandex.com>
2020-07-17 19:13:34 +0200

    removed config

--- source/secret.ts
+++
@@ -1,6 +0,0 @@
-
-
-export var sessionSecret:string = "eJ9u7wLpyg/M2weHl+VbNw"
-export var linkHost:string = "marchat.zapto.org:5565"
-export var projRoot:string = "/home/muffin/Documents/Javascript/discord-custom-embeds"
-export var allowUA:RegExp = /(linux|discordbot)/ig
